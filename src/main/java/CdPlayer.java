8655c27b9eaa1762c85652171917c197d3041d0d
nimbus117 <mail@nimbus117.co.uk>
2018-10-16 12:12:41 +0100

    added Component superclass and refactored record/cd player

--- src/main/java/CdPlayer.java
+++ src/main/java/CdPlayer.java
@@ -1,31 +1,11 @@
-public class CdPlayer {
+public class CdPlayer extends Component{
     private int numberOfCds;
-    private String make;
-    private String model;
 
     public CdPlayer(int numberOfCds, String make, String model) {
+        super(make, model);
        this.numberOfCds = numberOfCds;
-       this.make = make;
-       this.model = model;
     }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
     public int getNumberOfCds() {
         return numberOfCds;
-    }
-
-    public String play() {
-        return "Playing";
-    }
-
-    public String stop() {
-        return "Stopping";
     }
 }
--- src/main/java/RecordPlayer.java
+++ src/main/java/RecordPlayer.java
@@ -1,34 +1,15 @@
 import java.util.ArrayList;
 
-public class RecordPlayer {
+public class RecordPlayer extends Component {
     ArrayList<Integer> playSpeeds;
-    private String make;
-    private String model;
 
 
     public RecordPlayer(ArrayList<Integer> playSpeeds, String make, String model) {
+        super(make, model);
         this.playSpeeds = playSpeeds;
-        this.make = make;
-        this.model = model;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
     }
 
     public ArrayList getPlaySpeeds() {
         return playSpeeds;
-    }
-
-    public String play() {
-        return "Playing";
-    }
-
-    public String stop() {
-        return "Stopping";
     }
 }
